94d824d920e0fab32e8d8a4ffc764e494b4b3d89
yurim <yoorim0509@gmail.com>
2024-10-23 12:17:25 +0900

    1023 병합후 오류확인완료

--- be/src/main/java/com/allinone/proja3/controller/facilities/GymController.java
+++
@@ -1,4 +0,0 @@
-package com.allinone.proja3.proja3.controller.facilities;
-
-public class GymController {
-}
--- be/src/main/java/com/allinone/proja3/service/facilities/GymService.java
+++
@@ -1,4 +0,0 @@
-package com.allinone.proja3.proja3.service.facilities;
-
-public interface GymService {
-}
