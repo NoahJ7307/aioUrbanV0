c1c3d40b5463922d509dfba09d1cffbf4817f972
hyemiJ <166362402+hyemiJ@users.noreply.github.com>
2024-11-13 13:18:56 +0900

    카테고리

---
+++ fe/src/components/mileage/MyMileagePurchasePage.jsx
@@ -0,0 +1,5 @@
+const MyMileagePurchasePage = () => {
+    return <div>구매 내역 페이지</div>;
+};
+
+export default MyMileagePurchasePage;
---
+++ fe/src/components/mileage/MyMileageUsagePage.jsx
@@ -0,0 +1,5 @@
+const MyMileageUsagePage = () => {
+    return <div>사용 내역 페이지</div>;
+};
+
+export default MyMileageUsagePage;
